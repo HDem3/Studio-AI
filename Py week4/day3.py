483326b8e31c5f76daef8d2420c9d87248b117ee
HDem <93908678+HDem3@users.noreply.github.com>
2026-05-27 16:22:20 +0200

    esercizio calcolatrice con somma

--- Py week4/day3.py
+++ Py week4/day3.py
@@ -5,15 +5,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return "Home page"
+# @app.get("/")
+# def home():
+#     return "Home page"
+# 
+# @app.get("/pathpara/{id}")
+# def get_id(id: int):
+#     return {"path para": id}
+# 
+# @app.get("/querypara/")
+# def get_str(nome: str):
+#     return {"in query": nome}
 
-@app.get("/pathpara/{id}")
-def get_id(id: int):
-    return {"path para": id}
-
-@app.get("/querypara/")
-def get_str(nome: str):
-    return {"in query": nome}
-
+@app.get ("/calcolatrice/somma")
+def somma(a: float, b: float):
+    somma= a+b
+    return {"risultato": somma} 
