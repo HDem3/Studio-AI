2b53d81831cc43cd8a35ad376507486922e90bf2
HDem <93908678+HDem3@users.noreply.github.com>
2026-05-27 16:16:25 +0200

    esercizio server con query parametro e url parametro

---
+++ Py week4/day3.py
@@ -0,0 +1,19 @@
+# Parametro URL
+# Query param
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return "Home page"
+
+@app.get("/pathpara/{id}")
+def get_id(id: int):
+    return {"path para": id}
+
+@app.get("/querypara/")
+def get_str(nome: str):
+    return {"in query": nome}
+
