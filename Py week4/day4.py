a2f9db1aa9e1d01918001db65ddcc706984602de
HDem <93908678+HDem3@users.noreply.github.com>
2026-05-28 15:59:32 +0200

    funzione registrazione utente con email

--- Py week4/day4.py
+++ Py week4/day4.py
@@ -2,16 +2,31 @@
 # Post endpoint
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr # formatto email
 
 class Utente(BaseModel):
     nome: str
+    email: EmailStr
     eta: int
 
 
 app = FastAPI()
 
-@app.post("/utente")
-def crea_utente(user : Utente):
-    return { "msg":f"ciao {user.nome}" }
+# @app.post("/utente")
+# def crea_utente(user : Utente):
+#     return { "msg":f"ciao {user.nome}" }
+ 
 
+# Registrazione utenti API
+# Dati:
+# nome
+# email
+# età
+
+@app.post("/signup")
+def signup(user: Utente):
+    return {
+        "msg": f"Benvenuto {user.nome}!",
+        "email": user.email,
+        "eta": user.eta
+    }
