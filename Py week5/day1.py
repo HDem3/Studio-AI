1e13726d8a4f66ff0d2c64d4f0c9397eb6aa15f6
HDem <93908678+HDem3@users.noreply.github.com>
2026-06-05 16:37:00 +0200

    Aggiunto tabella studenti

--- Py week5/day1.py
+++ Py week5/day1.py
@@ -14,3 +14,23 @@ cur.execute("INSERT INTO todolist(task) VALUES ('palestra')")
 
 conn.commit()
 conn.close()
+
+conn= sqlite3.connect("studenti.db")
+cur= conn.cursor()
+cur.execute(
+    "CREATE TABLE IF NOT EXISTS studenti("
+    "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "nome TEXT,"
+    "cognome TEXT,"
+    "voto INTEGER)"
+)
+
+cur.execute("INSERT INTO studenti(nome,cognome,voto) "
+            "VALUES('Dem','H','18')")
+
+cur.execute("INSERT INTO studenti(nome,cognome,voto) "
+            "VALUES('Fabio','M','28')")
+
+conn.commit()
+conn.close()
+
