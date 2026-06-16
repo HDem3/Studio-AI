18e8d7d343e02daeb80aecfcf184b21e2201f3da
HDem <93908678+HDem3@users.noreply.github.com>
2026-06-16 16:44:28 +0200

    provato hash pwd con bcrypt e pbkdf2

--- Py week6/day1.py
+++ Py week6/day1.py
@@ -1,6 +1,43 @@
 import hashlib
+import bcrypt
+import os, time
 
-pwd = "ciao123"
-hashed = hashlib.sha256(pwd.encode()).hexdigest()
 
-print(hashed)
+pwd="my_secure_password"
+
+def hash_password_sha256(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+def hash_password_bcrypt(password):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt(rounds=11)).decode()
+
+def hash_password_pbkdf2(password, iterations=300_000):
+    return hashlib.pbkdf2_hmac("sha256", password.encode(), os.urandom(16), iterations).hex()
+
+
+print("SHA-256 Hash:", hash_password_sha256(pwd))
+print("bcrypt Hash:", hash_password_bcrypt(pwd))
+print("PBKDF2 Hash:", hash_password_pbkdf2(pwd))
+
+
+#
+#
+#pwd = b"password123"
+#salt = os.urandom(16)
+#
+#for iterations in [300_000]:
+#    start = time.time()
+#    hashlib.pbkdf2_hmac("sha256", pwd, salt, iterations)
+#    elapsed = (time.time() - start) * 1000
+#    print(f"{iterations} iterazioni: {elapsed:.2f} ms")
+#
+#
+#
+#
+#for cost in range(11, 12):
+#    start = time.time()
+#    bcrypt.hashpw(pwd, bcrypt.gensalt(rounds=cost))
+#    elapsed = (time.time() - start) * 1000
+#    print(f"Cost {cost}: {elapsed:.2f} ms")
+#
+#
